aeb887ebc82b81e6a1b593a4e1953988fe10970f
corentin-desilles <corentindesilles974@gmail.com>
2024-02-09 11:11:34 +0100

    waves

--- src/script.js
+++ src/script.js
@@ -5,9 +5,6 @@ import GUI from 'lil-gui'
 import waterVertexShader from './shaders/water/vertex.glsl'
 import waterFragmentShader from './shaders/water/fragment.glsl'
 
-console.log(waterFragmentShader)
-console.log(waterVertexShader)
-
 /**
  * Base
  */
@@ -27,7 +24,36 @@ const scene = new THREE.Scene()
 const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
 
 // Material
-const waterMaterial = new THREE.ShaderMaterial()
+const waterMaterial = new THREE.ShaderMaterial({
+    vertexShader: waterVertexShader,
+    fragmentShader: waterFragmentShader,
+    uniforms: {
+        uTime: { value: 0 },
+        uBigWavesElevation: { value: 0.2 },
+        uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
+    },
+})
+
+// debug material
+//waves elevation
+gui.add(waterMaterial.uniforms.uBigWavesElevation, 'value')
+    .min(0)
+    .max(1)
+    .step(0.001)
+    .name('uBigWavesElevation')
+
+//waves frequency X and Y
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x')
+    .min(0)
+    .max(10)
+    .step(0.001)
+    .name('uBigWavesFrequencyX')
+
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y')
+    .min(0)
+    .max(10)
+    .step(0.001)
+    .name('uBigWavesFrequencyY')
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
@@ -89,6 +115,9 @@ const clock = new THREE.Clock()
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update water
+    waterMaterial.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
